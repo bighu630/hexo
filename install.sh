b962f77daadc1048a0614b6605595093fe125c76
ivhu <ivhu@foxmail.com>
2024-09-03 10:31:26 +0800

    updata:更新hexo配置

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-npm install -g hexo-cli
+npm install -g hexo-cli --save
 npm i hexo-theme-volantis --save
 npm install hexo-deployer-git --save
 npm install https://github.com/CodeFalling/hexo-asset-image --save
