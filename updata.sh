15212b502b51c344930d269e19da986a860ea396
ivhu <ivhu@foxmail.com>
2025-04-16 20:36:39 +0800

    update

--- updata.sh
+++ updata.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 git pull
-git add *
-git commit -m "updata:更新hexo配置"
+git add .
+git commit -m "update"
 git push
+
+cd public
+git pull
+cd ..
+npx hexo g
+
+cd public
+git add .
+git commit -m "update"
+git push
+cd ..
